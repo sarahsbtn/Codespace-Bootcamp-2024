a2830b371a43f38c9a2f84bff6851a7ce6dcc9b1
Sarah <149964758+sarahsbtn@users.noreply.github.com>
2024-10-23 02:54:17 +0100

    Update guess-number-game-script.js

--- Testing Technologies and Programming/Resources-Code-Challenge-4/guess-number-game-script.js
+++ Testing Technologies and Programming/Resources-Code-Challenge-4/guess-number-game-script.js
@@ -1,9 +1,13 @@
-// Generate a random number between 1 and 10
-let randomNumber = Math.floor(Math.random() * 10) + 1; /* Math.random to generate number between 0 and 1
-                                                          * that by 10 to adjust range to between 0 and just under 10
-                                                          Math.floor rounds the random number to nearest whole number
-                                                          + 1 to adjust range to between 1 and 10
-                                                        */
+// Function to generate a random number between 1 and 10
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+/* Math.random to generate number between 0 and 1
+* that by 10 to adjust range to between 0 and just under 10
+Math.floor rounds the random number to nearest whole number
++ 1 to adjust range to between 1 and 10
+*/
 
 // Set maximum number of attempts allowed for each game session (3)
 let attempts = 0;
@@ -13,6 +17,9 @@ const maxAttempts = 3;
 const submitButton = document.querySelector('button');
 const playerGuessInput = document.getElementById('playerGuess');
 let gameText = document.getElementById('gameText');
+
+// Generate the random number
+let randomNumber = generateRandomNumber();
 
 // Add event listener to submit button
 submitButton.addEventListener('click', () => {
@@ -57,11 +64,11 @@ submitButton.addEventListener('click', () => {
     }
 });
 
-// Function to reset the game state
+// Function to reset the game 
 function resetGame() {
 
     // Generate a new random number
-    randomNumber = Math.floor(Math.random() * 10) + 1; 
+    randomNumber = generateRandomNumber();
 
     // Reset the number of attempt back to 0
     attempts = 0; 
